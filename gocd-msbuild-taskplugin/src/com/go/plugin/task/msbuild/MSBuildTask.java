d6971e992bce0b30fb60d7104ee49393099a68e7
Andrew Kanyer <andy.kanyer@gmail.com>
2015-03-25 12:39:28 -0500

    Initial Commit

---
+++ gocd-msbuild-taskplugin/src/com/go/plugin/task/msbuild/MSBuildTask.java
@@ -0,0 +1,119 @@
+package com.go.plugin.task.msbuild;
+
+import com.thoughtworks.go.plugin.api.annotation.Extension;
+import com.thoughtworks.go.plugin.api.response.validation.ValidationError;
+import com.thoughtworks.go.plugin.api.response.validation.ValidationResult;
+import com.thoughtworks.go.plugin.api.task.Task;
+import com.thoughtworks.go.plugin.api.task.TaskConfig;
+import com.thoughtworks.go.plugin.api.task.TaskExecutor;
+import com.thoughtworks.go.plugin.api.task.TaskView;
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
+@Extension
+public class MSBuildTask implements Task {
+	public static final String CUSTOMIZEMSBUILDPATH = "CustomizeMSBuildPath"; 
+    public static final String MSBUILDPATH = "MSBuildPath";
+    public static final String SOLUTIONFILE = "SolutionFile";
+    public static final String PROPERTIES = "Properties";
+    public static final String VERBOSITY = "Verbosity";
+    public static final String SPECIFYTARGETS = "SpecifyTargets";
+    public static final String TARGETS = "Targets";
+    public static final String DETAILEDSUMMARY = "DetailedSummary";
+    public static final String NOLOGO = "NoLogo";
+    public static final String NOAUTORESPONSE = "NoAutoResponse";
+
+    @Override
+    public TaskConfig config() {
+        TaskConfig config = new TaskConfig();
+        config.addProperty(CUSTOMIZEMSBUILDPATH);
+        config.addProperty(MSBUILDPATH);
+        config.addProperty(SOLUTIONFILE);
+        
+        config.addProperty(PROPERTIES);
+        config.addProperty(VERBOSITY);
+        config.addProperty(SPECIFYTARGETS);
+        config.addProperty(TARGETS);
+        
+        config.addProperty(DETAILEDSUMMARY);
+        config.addProperty(NOLOGO);
+        config.addProperty(NOAUTORESPONSE);
+        return config;
+    }
+
+    @Override
+    public TaskExecutor executor() {
+        return new MSBuildTaskExecutor();
+    }
+
+    @Override
+    public TaskView view() {
+        TaskView taskView = new TaskView() {
+            @Override
+            public String displayValue() {
+                return "MSBuild";
+            }
+
+            @Override
+            public String template() {
+                try {
+                    return IOUtils.toString(getClass().getResourceAsStream("/resources/views/task.template.html"), "UTF-8");
+                } catch (Exception e) {
+                    return "Failed to find template: " + e.getMessage();
+                }
+            }
+        };
+        return taskView;
+    }
+
+    @Override
+    public ValidationResult validate(TaskConfig configuration) {
+        ValidationResult validationResult = new ValidationResult();
+        
+        //String customizemsbuildpath = configuration.getValue(CUSTOMIZEMSBUILDPATH);
+        //if(customizemsbuildpath.equals("true")) {
+        	String msbuildpath = configuration.getValue(MSBUILDPATH);
+        	if(StringUtils.isBlank(msbuildpath)) {
+        		validationResult.addError(new ValidationError(MSBUILDPATH, "Path to MSBuild.exe must be specified"));
+        	}
+        //}
+        
+        String solutionfile = configuration.getValue(SOLUTIONFILE);
+        if (StringUtils.isBlank(solutionfile)) {
+            validationResult.addError(new ValidationError(SOLUTIONFILE, "A Solution file must be specified"));
+        }
+        
+        String properties = configuration.getValue(PROPERTIES);
+        if (!StringUtils.isBlank(properties) && !propertiesValid(properties)) {
+        	validationResult.addError(new ValidationError(PROPERTIES, "Invalid entry for Properties - make sure one property per line formatted like propName=propValue"));
+        }
+        
+        String specifyTargets = configuration.getValue(SPECIFYTARGETS);
+        if(specifyTargets.equals("true")) {
+        	String targets = configuration.getValue(TARGETS);
+        	if(StringUtils.isBlank(targets)) {
+        		validationResult.addError(new ValidationError(TARGETS, "If SpecifyTargets is checked, Targets cannot be empty."));
+        	}
+        }
+        
+        return validationResult;
+    }
+    
+    protected boolean propertiesValid(String properties){
+    	//split properties on newline
+    	String props[] = properties.split("\\r?\\n");
+    	Pattern regex = Pattern.compile("\\w+=\\w+");
+    	for(String prop : props) {
+    		//strip all whitespace
+    		prop = prop.replaceAll("\\s", ""); 
+    		Matcher matcher = regex.matcher(prop);
+    		
+    		if(!matcher.matches()) 
+    			return false;    	
+    	}
+    	return true;
+    }
+}
---
+++ gocd-msbuild-taskplugin/src/com/go/plugin/task/msbuild/MSBuildTaskExecutor.java
@@ -0,0 +1,111 @@
+package com.go.plugin.task.msbuild;
+
+import com.thoughtworks.go.plugin.api.response.execution.ExecutionResult;
+import com.thoughtworks.go.plugin.api.task.*;
+import com.thoughtworks.go.plugin.api.task.Console;
+import org.apache.commons.lang3.StringUtils;
+
+import java.io.*;
+import java.util.*;
+
+public class MSBuildTaskExecutor implements TaskExecutor {
+
+    @Override
+    public ExecutionResult execute(TaskConfig taskConfig, TaskExecutionContext taskExecutionContext) {
+        ProcessBuilder powershell = createMSBuildCommand(taskExecutionContext, taskConfig);
+
+        Console console = taskExecutionContext.console();
+        console.printLine("Launching command: " + StringUtils.join(powershell.command(), " "));
+
+        try {
+            Process process = powershell.start();
+
+            //PutScriptIntoPowershellStdin(taskExecutionContext, taskConfig, process);
+
+            console.readErrorOf(process.getErrorStream());
+            console.readOutputOf(process.getInputStream());
+
+            int exitCode = process.waitFor();
+            process.destroy();
+
+            if (exitCode != 0) {
+                return ExecutionResult.failure("Build Failure");
+            }
+        }
+        catch(Exception e) {
+            console.printLine(e.getMessage());
+            return ExecutionResult.failure("Failed while running MSBuild task ", e);
+        }
+
+        return ExecutionResult.success("Build Success");
+    }
+
+    ProcessBuilder createMSBuildCommand(TaskExecutionContext taskContext, TaskConfig taskConfig) {
+        String msBuildPath = "C:\\Windows\\Microsoft.NET\\Framework\\v4.0.30319\\MSBuild.exe";
+
+        List<String> command = new ArrayList<String>();
+        /**if(taskConfig.getValue(MSBuildTask.CUSTOMIZEMSBUILDPATH).equals("true")) {
+        	msBuildPath = taskConfig.getValue(MSBuildTask.MSBUILDPATH);
+        }
+        **/
+        msBuildPath = taskConfig.getValue(MSBuildTask.MSBUILDPATH);
+        command.add(msBuildPath);
+        
+        AddMSBuildArguments(taskConfig, command);
+        AddProjectFile(taskConfig, command);
+
+        ProcessBuilder processBuilder = new ProcessBuilder(command);
+
+        processBuilder.directory(new File(taskContext.workingDir()));
+        return processBuilder;
+    }
+
+    private void AddProjectFile(TaskConfig taskConfig, List<String> command) {
+        String solutionFile = taskConfig.getValue(MSBuildTask.SOLUTIONFILE);
+        command.add(solutionFile);
+    }
+
+    private void AddMSBuildArguments(TaskConfig taskConfig, List<String> command) {
+        String rawProperties = taskConfig.getValue(MSBuildTask.PROPERTIES);
+        String verbosity = taskConfig.getValue(MSBuildTask.VERBOSITY);
+        String specifyTargets = taskConfig.getValue(MSBuildTask.SPECIFYTARGETS);
+        
+    	String detailedSummary = taskConfig.getValue(MSBuildTask.DETAILEDSUMMARY);
+        String noLogo = taskConfig.getValue(MSBuildTask.NOLOGO);
+        String noAutoResponse = taskConfig.getValue(MSBuildTask.NOAUTORESPONSE);
+
+        if(rawProperties != null && !StringUtils.isEmpty(rawProperties)) {
+        	//split props by line
+        	String properties[] = rawProperties.split("[\r\n]+"); 
+        	for(String prop : properties){
+        		//strip any whitespace from property leaving only 'propertyName'='value'
+        		prop = prop.replaceAll("\\s+", ""); 
+        		command.add("/property:"+prop);
+        	}
+        }
+        
+        if(verbosity != null && !StringUtils.isEmpty(verbosity)) {
+        	command.add("/verbosity:" + verbosity);
+        } else {
+        	command.add("/verbosity:normal");
+        }
+        
+        if(specifyTargets != null && specifyTargets.equals("true")) {
+        	String targets = taskConfig.getValue(MSBuildTask.TARGETS);
+        	if(targets != null &&  !StringUtils.isEmpty(targets)) {
+        		targets = targets.replaceAll("\\s+", "");
+        		command.add("/targets:"+targets);
+        	}
+        }
+        
+        if (detailedSummary != null && detailedSummary.equals("true")) {
+            command.add("/detailedsummary");
+        }
+        if (noLogo != null && noLogo.equals("true")) {
+            command.add("/nologo");
+        }
+        if (noAutoResponse != null && noAutoResponse.equals("true")) {
+            command.add("/noautoResponse");
+        }
+    }
+}
